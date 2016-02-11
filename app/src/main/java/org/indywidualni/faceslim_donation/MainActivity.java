61a8aa9bc33fa245f920f993b51d77a9a8566879
indywidualny <graal13>
2016-02-11 06:09:54 +0100

    Missing files

--- app/src/main/java/org/indywidualni/faceslim_donation/MainActivity.java
+++ app/src/main/java/org/indywidualni/faceslim_donation/MainActivity.java
@@ -1,13 +1,21 @@
 package org.indywidualni.faceslim_donation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitle(" " + getString(R.string.app_name));
+        toolbar.setTitleTextColor(Color.WHITE);
     }
 }
