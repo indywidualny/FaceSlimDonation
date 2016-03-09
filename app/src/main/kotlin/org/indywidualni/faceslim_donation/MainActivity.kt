bd4b2c3b9e36ccea67a51c0fe5477bdad1211fa8
indywidualny <graal13>
2016-03-09 19:54:08 +0100

    Convert Java code to Kotlin

---
+++ app/src/main/kotlin/org/indywidualni/faceslim_donation/MainActivity.kt
@@ -0,0 +1,20 @@
+package org.indywidualni.faceslim_donation
+
+import android.app.Activity
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+
+        toolbar.setLogo(R.mipmap.ic_launcher)
+        toolbar.title = " " + getString(R.string.app_name)
+        toolbar.setTitleTextColor(Color.WHITE)
+    }
+}
